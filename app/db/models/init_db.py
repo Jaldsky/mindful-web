6a26f56186848dd60665c49e2397b2eebdeec2f9
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-10-03 21:21:07 +0300

    Add safe tables init

--- app/db/models/init_db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.engine import Engine
-from .base import Base
-
-
-def init_db(engine: Engine) -> None:
-    from . import tables  # noqa: F401
-
-    Base.metadata.create_all(bind=engine)
