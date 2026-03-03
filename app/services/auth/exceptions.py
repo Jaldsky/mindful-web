4eac85238c86224ec25d1b999c74ea657327a6ce
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2026-03-03 18:07:13 +0300

    Add schemas for oauth_login + normalizers + validators

--- app/services/auth/exceptions.py
+++ app/services/auth/exceptions.py
@@ -1,4 +1,5 @@
 from ..exceptions import (
+    BadRequestException,
     UnauthorizedException,
     ForbiddenException,
     ConflictException,
@@ -6,6 +7,19 @@ from ..exceptions import (
     InternalServerErrorException,
 )
 from ...schemas.auth.auth_error_code import AuthErrorCode
+
+
+# Исключения для 400
+class OAuthProviderUnsupportedException(BadRequestException):
+    """Неподдерживаемый OAuth провайдер (400)."""
+
+    error_code = AuthErrorCode.OAUTH_PROVIDER_UNSUPPORTED
+
+
+class OAuthRedirectUriMismatchException(BadRequestException):
+    """Передан неподходящий redirect_uri для OAuth (400)."""
+
+    error_code = AuthErrorCode.OAUTH_REDIRECT_URI_MISMATCH
 
 
 # Исключения для 401
@@ -37,6 +51,18 @@ class UserNotFoundException(UnauthorizedException):
     """Пользователь не найден в системе (401)."""
 
     error_code = AuthErrorCode.USER_NOT_FOUND
+
+
+class OAuthCodeExchangeFailedException(UnauthorizedException):
+    """Не удалось обменять код OAuth на токены (401)."""
+
+    error_code = AuthErrorCode.OAUTH_CODE_EXCHANGE_FAILED
+
+
+class OAuthStateInvalidException(UnauthorizedException):
+    """State OAuth невалиден или отсутствует (401)."""
+
+    error_code = AuthErrorCode.OAUTH_STATE_INVALID
 
 
 # Исключения для 403
@@ -90,10 +116,46 @@ class TooManyAttemptsException(UnprocessableEntityException):
     error_code = AuthErrorCode.TOO_MANY_ATTEMPTS
 
 
+class OAuthEmailMissingException(UnprocessableEntityException):
+    """Провайдер не вернул email (422)."""
+
+    error_code = AuthErrorCode.OAUTH_EMAIL_MISSING
+
+
+class OAuthEmailNotVerifiedException(UnprocessableEntityException):
+    """Email у OAuth провайдера не подтвержден (422)."""
+
+    error_code = AuthErrorCode.OAUTH_EMAIL_NOT_VERIFIED
+
+
 class InvalidVerificationCodeFormatException(UnprocessableEntityException):
     """Неверный формат кода подтверждения (422)."""
 
     error_code = AuthErrorCode.INVALID_VERIFICATION_CODE
+
+
+class InvalidOAuthCodeFormatException(UnprocessableEntityException):
+    """Неверный формат OAuth authorization code (422)."""
+
+    error_code = AuthErrorCode.INVALID_OAUTH_CODE_FORMAT
+
+
+class InvalidOAuthRedirectUriFormatException(UnprocessableEntityException):
+    """Неверный формат OAuth redirect URI (422)."""
+
+    error_code = AuthErrorCode.INVALID_OAUTH_REDIRECT_URI_FORMAT
+
+
+class InvalidOAuthStateFormatException(UnprocessableEntityException):
+    """Неверный формат OAuth state (422)."""
+
+    error_code = AuthErrorCode.INVALID_OAUTH_STATE_FORMAT
+
+
+class InvalidOAuthCodeVerifierFormatException(UnprocessableEntityException):
+    """Неверный формат OAuth PKCE code_verifier (422)."""
+
+    error_code = AuthErrorCode.INVALID_OAUTH_CODE_VERIFIER_FORMAT
 
 
 class VerificationCodeExpiredException(UnprocessableEntityException):
