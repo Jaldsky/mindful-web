883a2904d35f45f2e9719514b8e01668188495e7
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2026-03-07 16:17:11 +0300

    Add oauth types

--- app/services/auth/types.py
+++ app/services/auth/types.py
@@ -8,3 +8,10 @@ RefreshToken: TypeAlias = str
 TokenPayload: TypeAlias = dict[str, Any]
 
 SessionStatus: TypeAlias = Literal["authenticated", "anonymous", "none"]
+
+OAuthProviderName: TypeAlias = str
+OAuthAuthorizationCode: TypeAlias = str
+OAuthRedirectUri: TypeAlias = str
+OAuthCodeVerifier: TypeAlias = str
+OAuthIdToken: TypeAlias = str
+OAuthProviderSubject: TypeAlias = str
